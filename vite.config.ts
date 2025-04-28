ba4270e28acadc05d2ae90fd9364ceb6812740e9
Joe Allan Muharsky <joemu@highlandscastleholdings.com>
2025-04-28 14:39:22 -0700

    Deploy as GitHub pages to project site.

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,18 @@ import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config https://vitest.dev/config
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  test: {
-    globals: true,
-    environment: 'happy-dom',
-    setupFiles: '.vitest/setup',
-    include: ['**/test.{ts,tsx}', '**/*.test.{ts,tsx}']
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === 'production' ? '/TODD/' : '/',
+    plugins: [react(), tsconfigPaths()],
+    build: {
+      outDir: 'TODD'
+    },
+    test: {
+      globals: true,
+      environment: 'happy-dom',
+      setupFiles: '.vitest/setup',
+      include: ['**/test.{ts,tsx}', '**/*.test.{ts,tsx}']
+    }
   }
 })
