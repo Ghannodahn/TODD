b45e60442fdbd7700228bcd09b227a883ff3dc99
Joe Allan Muharsky <joemu@highlandscastleholdings.com>
2025-05-05 23:24:16 -0700

    Fixed casing issue in tailwind config.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 export default {
-  content: ['./src/**/*.{mjs,js,ts,jsx,tsx}', './src/data/Recipes/**/*.json'],
+  content: ['./src/**/*.{mjs,js,ts,jsx,tsx}', './src/data/recipes/**/*.json'],
   theme: {
     extend: {}
   },
